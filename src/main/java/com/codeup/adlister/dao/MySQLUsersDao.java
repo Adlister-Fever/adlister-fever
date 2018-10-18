3c3f3e873be2c734ed9473d0c0330714db0cecc5
chrisesquerra <esquerra>
2018-10-18 17:05:43 -0500

    init

---
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -0,0 +1,70 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.Config;
+import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.*;
+
+public class MySQLUsersDao implements Users {
+    private Connection connection;
+
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUser(),
+                config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+
+    @Override
+    public User findByUsername(String username) {
+        String query = "SELECT * FROM users WHERE username = ? LIMIT 1";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, username);
+            return extractUser(stmt.executeQuery());
+        } catch (SQLException e) {
+            throw new RuntimeException("Error finding a user by username", e);
+        }
+    }
+
+    @Override
+    public Long insert(User user) {
+        String query = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+        try {
+            String userPass = user.getPassword();
+            String hashPass = BCrypt.hashpw(userPass, BCrypt.gensalt());
+            PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, hashPass);
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating new user", e);
+        }
+    }
+
+    private User extractUser(ResultSet rs) throws SQLException {
+        if (! rs.next()) {
+            return null;
+        }
+        return new User(
+            rs.getLong("user_id"),
+            rs.getString("username"),
+            rs.getString("email"),
+            rs.getString("password")
+        );
+    }
+
+}
