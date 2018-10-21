c98ecdfe62c1322232cbf8ea3f10476d81625023
wilfredopacheco <wilfredo.pacheco.sa@gmail.com>
2018-10-21 14:49:33 -0500

    fixed ads.sql connection with ads and users, populated ads to main ads index

--- src/main/java/com/codeup/adlister/dao/ListAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/ListAdsDao.java
@@ -1,34 +1,34 @@
-//package com.codeup.adlister.dao;
-//
-//import com.codeup.adlister.models.Ad;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class ListAdsDao implements Ads {
-//    private List<Ad> ads;
-//
-//    public List<Ad> all() {
-//        if (ads == null) {
-//            ads = generateAds();
-//        }
-//        return ads;
-//    }
-//
-//    public Long insert(Ad ad) {
-//        // make sure we have ads
-//        if (ads == null) {
-//            ads = generateAds();
-//        }
-//        // we'll assign an "id" here based on the size of the ads list
-//        // really the dao would handle this
-//        ad.setId((long) ads.size());
-//        ads.add(ad);
-//        return ad.getId();
-//    }
-//
-//    private List<Ad> generateAds() {
-//        List<Ad> ads = new ArrayList<>();
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.Ad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListAdsDao implements Ads {
+    private List<Ad> ads;
+
+    public List<Ad> all() {
+        if (ads == null) {
+            ads = generateAds();
+        }
+        return ads;
+    }
+
+    public Long insert(Ad ad) {
+        // make sure we have ads
+        if (ads == null) {
+            ads = generateAds();
+        }
+        // we'll assign an "id" here based on the size of the ads list
+        // really the dao would handle this
+        ad.setId((long) ads.size());
+        ads.add(ad);
+        return ad.getId();
+    }
+
+    private List<Ad> generateAds() {
+        List<Ad> ads = new ArrayList<>();
 //        ads.add(new Ad(
 //            1,
 //            1,
@@ -53,6 +53,6 @@
 //            "JavaScript Developer needed",
 //            "Must have strong Java skills"
 //        ));
-//        return ads;
-//    }
-//}
+        return ads;
+    }
+}
